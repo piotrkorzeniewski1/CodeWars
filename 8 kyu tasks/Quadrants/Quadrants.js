0fd0e5482fd65bd393b1dc6ab765eeffd38bdae8
unknown <piotrek.korzeniewski@wp.pl>
2023-10-27 19:06:15 +0200

    First task

---
+++ 8 kyu tasks/Quadrants/Quadrants.js
@@ -0,0 +1,20 @@
+function quadrant(x, y) {
+  let result;
+  if(x>0&&y>0)
+  {
+    result=1;
+  }
+  else if(x<0&&y>0)
+  {
+      result = 2;
+  }
+  else if(x<0&&y<0)
+  {
+    result=3;
+  }
+  else
+  {
+    result = 4;
+  }
+    return result;
+}
